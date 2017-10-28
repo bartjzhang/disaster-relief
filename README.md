1f1cbf5130ea6b75bfcc2e2f55d7c46c413615bb
Kamil Ali <kamil.m.ali@outlook.com>
2017-10-28 16:19:53 -0500

    Added docker-compose yaml

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ Tool that helps first responders better visualize areas of disaster.
 To run:
 
 ```sh
-docker build -t disaster-relief-app:latest .
-docker run -d -p 5000:5000 disaster-relief-app
+docker-compose up
 ```
--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Nothing yet'
+    return 'Nothing yet, but working on it'
+
+@app.route('/api/v1/status', methods=['GET'])
+def get_status():
+	return jsonify({'status': 'Live in full effect!'})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
